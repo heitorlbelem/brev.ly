feecb8b4a2c323fae14f8199b972249866212aa2
Heitor Belém <heitorlbelem98@gmail.com>
2025-05-13 10:09:50 -0300

    Configures fastify swagger

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,13 +1,40 @@
 import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
 
 const server = fastify()
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      error: 'Validation error',
+      message: error.validation,
+    })
+  }
+  // Send error to Sentry/Datadog
+  console.error(error)
+  return reply.status(500).send({ message: 'Internal server error' })
+})
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Brev.ly API Documentation',
+      version: '1.0.0',
+    },
+  },
+})
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
 
 server.register(fastifyCors, { origin: '*' })
-
-server.get('/', async (request, reply) => {
-  reply.send({ hello: 'world' })
-})
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP Server running!')
