541a31c9ad28b435b047e5f8390b65c7c68793ec
Heitor Belém <heitorlbelem98@gmail.com>
2025-05-21 22:16:47 -0300

    [Backend] Linting files and changing returned json

--- server/src/infra/http/routes/get-all-shortened-urls.route.ts
+++ server/src/infra/http/routes/get-all-shortened-urls.route.ts
@@ -1,43 +1,47 @@
-import { getAllShortenedUrls } from '@/app/functions/get-all-shortened-urls'
-import { getAllShortenedUrlsSchema } from '@/app/schemas/get-shortened-urls-schema'
-import { isRight, unwrapEither } from '@/shared/either'
-import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
-import { z } from 'zod'
+import { getAllShortenedUrls } from "@/app/functions/get-all-shortened-urls";
+import { getAllShortenedUrlsSchema } from "@/app/schemas/get-shortened-urls-schema";
+import { isRight, unwrapEither } from "@/shared/either";
+import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import { z } from "zod";
 
-export const getAllShortenedUrlsRoute: FastifyPluginAsyncZod = async server => {
-  server.get(
-    '/shortened-urls',
-    {
-      schema: {
-        summary: 'Get all shortened URLs',
-        tags: ['Shortened URLs'],
-        querystring: getAllShortenedUrlsSchema,
-        response: {
-          200: z
-            .object({
-              nextCursor: z.string().nullable(),
-              urls: z.array(
-                z.object({
-                  id: z.string(),
-                  originalUrl: z.string(),
-                  shortenedUrl: z.string(),
-                  accessesCount: z.number(),
-                  createdAt: z.date(),
-                })
-              ),
-            })
-            .describe('Shortened URLs paginated list'),
-        },
-      },
-    },
-    async (request, reply) => {
-      const { cursor, pageSize } = request.query
-      const result = await getAllShortenedUrls({ cursor, pageSize })
+export const getAllShortenedUrlsRoute: FastifyPluginAsyncZod = async (
+	server,
+) => {
+	server.get(
+		"/shortened-urls",
+		{
+			schema: {
+				summary: "Get all shortened URLs",
+				tags: ["Shortened URLs"],
+				querystring: getAllShortenedUrlsSchema,
+				response: {
+					200: z
+						.object({
+							meta: z.object({
+								nextCursor: z.string().nullable(),
+							}),
+							urls: z.array(
+								z.object({
+									id: z.string(),
+									originalUrl: z.string(),
+									shortenedUrl: z.string(),
+									accessesCount: z.number(),
+									createdAt: z.date(),
+								}),
+							),
+						})
+						.describe("Shortened URLs paginated list"),
+				},
+			},
+		},
+		async (request, reply) => {
+			const { cursor, pageSize } = request.query;
+			const result = await getAllShortenedUrls({ cursor, pageSize });
 
-      if (isRight(result)) {
-        const { nextCursor, urls } = unwrapEither(result)
-        return reply.status(200).send({ nextCursor, urls })
-      }
-    }
-  )
-}
+			if (isRight(result)) {
+				const { meta, urls } = unwrapEither(result);
+				return reply.status(200).send({ meta, urls });
+			}
+		},
+	);
+};
--- server/src/infra/http/server.ts
+++ server/src/infra/http/server.ts
@@ -1,53 +1,56 @@
-import { fastifyCors } from '@fastify/cors'
-import { fastifySwagger } from '@fastify/swagger'
-import { fastifySwaggerUi } from '@fastify/swagger-ui'
-import { fastify } from 'fastify'
+import { fastifyCors } from "@fastify/cors";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import { fastify } from "fastify";
 import {
-  hasZodFastifySchemaValidationErrors,
-  jsonSchemaTransform,
-  serializerCompiler,
-  validatorCompiler,
-} from 'fastify-type-provider-zod'
-import { accessShortenedUrlRoute } from './routes/access-shortened-url.route'
-import { createShortenedUrlRoute } from './routes/create-shortened-url.route'
-import { deleteShortenedUrlRoute } from './routes/delete-shortened-url.route'
-import { exportAllShortenedUrlsRoute } from './routes/export-shortened-urls.route'
-import { getAllShortenedUrlsRoute } from './routes/get-all-shortened-urls.route'
-import { getShortenedUrlRoute } from './routes/get-shortened-url.route'
+	hasZodFastifySchemaValidationErrors,
+	jsonSchemaTransform,
+	serializerCompiler,
+	validatorCompiler,
+} from "fastify-type-provider-zod";
+import { accessShortenedUrlRoute } from "./routes/access-shortened-url.route";
+import { createShortenedUrlRoute } from "./routes/create-shortened-url.route";
+import { deleteShortenedUrlRoute } from "./routes/delete-shortened-url.route";
+import { exportAllShortenedUrlsRoute } from "./routes/export-shortened-urls.route";
+import { getAllShortenedUrlsRoute } from "./routes/get-all-shortened-urls.route";
+import { getShortenedUrlRoute } from "./routes/get-shortened-url.route";
 
-const server = fastify()
-server.setValidatorCompiler(validatorCompiler)
-server.setSerializerCompiler(serializerCompiler)
+const server = fastify();
+server.setValidatorCompiler(validatorCompiler);
+server.setSerializerCompiler(serializerCompiler);
 server.setErrorHandler((error, request, reply) => {
-  if (hasZodFastifySchemaValidationErrors(error)) {
-    return reply.status(400).send({
-      message: error.validation.map(e => e.message)[0],
-    })
-  }
-  console.error(error)
-  return reply.status(500).send({ message: 'Internal server error' })
-})
+	if (hasZodFastifySchemaValidationErrors(error)) {
+		return reply.status(400).send({
+			message: error.validation.map((e) => e.message)[0],
+		});
+	}
+	console.error(error);
+	return reply.status(500).send({ message: "Internal server error" });
+});
 server.register(fastifySwagger, {
-  openapi: {
-    info: {
-      title: 'Brev.ly API Documentation',
-      version: '1.0.0',
-    },
-  },
-  transform: jsonSchemaTransform,
-})
+	openapi: {
+		info: {
+			title: "Brev.ly API Documentation",
+			version: "1.0.0",
+		},
+	},
+	transform: jsonSchemaTransform,
+});
 server.register(fastifySwaggerUi, {
-  routePrefix: '/docs',
-})
-server.register(fastifyCors, { origin: '*' })
+	routePrefix: "/docs",
+});
+server.register(fastifyCors, {
+	origin: "http://localhost:5173",
+	credentials: true,
+});
 
-server.register(createShortenedUrlRoute)
-server.register(getAllShortenedUrlsRoute)
-server.register(getShortenedUrlRoute)
-server.register(deleteShortenedUrlRoute)
-server.register(accessShortenedUrlRoute)
-server.register(exportAllShortenedUrlsRoute)
+server.register(createShortenedUrlRoute);
+server.register(getAllShortenedUrlsRoute);
+server.register(getShortenedUrlRoute);
+server.register(deleteShortenedUrlRoute);
+server.register(accessShortenedUrlRoute);
+server.register(exportAllShortenedUrlsRoute);
 
-server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
-  console.log('HTTP Server running!')
-})
+server.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
+	console.log("HTTP Server running!");
+});
