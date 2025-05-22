6af108324664a90f43e2cbfcd562e3367be3594b
Heitor Belém <heitorlbelem98@gmail.com>
2025-05-21 21:59:20 -0300

    [Backend] Change response format for get shortened urls request

--- server/src/app/functions/get-all-shortened-urls.ts
+++ server/src/app/functions/get-all-shortened-urls.ts
@@ -1,36 +1,40 @@
-import { db } from '@/infra/db'
-import { schema } from '@/infra/db/schemas'
-import { type Either, makeRight } from '@/shared/either'
-import { gt } from 'drizzle-orm'
-import type { z } from 'zod'
-import { getAllShortenedUrlsSchema } from '../schemas/get-shortened-urls-schema'
+import { db } from "@/infra/db";
+import { schema } from "@/infra/db/schemas";
+import { type Either, makeRight } from "@/shared/either";
+import { gt } from "drizzle-orm";
+import type { z } from "zod";
+import { getAllShortenedUrlsSchema } from "../schemas/get-shortened-urls-schema";
 
-type GetAllShortenedUrlsInput = z.infer<typeof getAllShortenedUrlsSchema>
+type GetAllShortenedUrlsInput = z.infer<typeof getAllShortenedUrlsSchema>;
 
 type GetAllShortenedUrlsOutput = {
-  nextCursor: string | null
-  urls: {
-    id: string
-    originalUrl: string
-    shortenedUrl: string
-    createdAt: Date
-    accessesCount: number
-  }[]
-}
+	meta: {
+		nextCursor: string | null;
+	};
+	urls: {
+		id: string;
+		originalUrl: string;
+		shortenedUrl: string;
+		createdAt: Date;
+		accessesCount: number;
+	}[];
+};
 
 export async function getAllShortenedUrls(
-  input: GetAllShortenedUrlsInput
+	input: GetAllShortenedUrlsInput,
 ): Promise<Either<never, GetAllShortenedUrlsOutput>> {
-  const { cursor, pageSize } = getAllShortenedUrlsSchema.parse(input)
-  const whereClause = cursor ? gt(schema.shortenedUrls.id, cursor) : undefined
-  const results = await db
-    .select()
-    .from(schema.shortenedUrls)
-    .where(whereClause)
-    .limit(pageSize)
-  const nextCursor = results.length > 0 ? results[results.length - 1].id : null
-  return makeRight({
-    urls: results,
-    nextCursor,
-  })
+	const { cursor, pageSize } = getAllShortenedUrlsSchema.parse(input);
+	const whereClause = cursor ? gt(schema.shortenedUrls.id, cursor) : undefined;
+	const results = await db
+		.select()
+		.from(schema.shortenedUrls)
+		.where(whereClause)
+		.limit(pageSize);
+	const nextCursor = results.length > 0 ? results[results.length - 1].id : null;
+	return makeRight({
+		urls: results,
+		meta: {
+			nextCursor,
+		},
+	});
 }
